0733e8708bbc84dc19b9cc820f738cb9545430da
CyaMate420 <98167926+CyaMate420@users.noreply.github.com>
2022-01-25 17:17:15 +0100

    .ino Datei richtig vorbereitet

--- 3DLaserScanner_VL8x_main/Webserver_VL8X_Skeleton/Webserver_VL8X_Skeleton.ino
+++ 3DLaserScanner_VL8x_main/Webserver_VL8X_Skeleton/Webserver_VL8X_Skeleton.ino
@@ -1,107 +1,91 @@
-
 #include <WiFi.h>
 #include <WebServer.h>
 
 /*______________Setup Server on ESP_______________________________________________________ */
-const char* ssid = "LaserScanner";  //Server name/SSID
-const char* password = "3desp32";   //Server password
-IPAddress local_ip(192,168,1,1);    //Sever default ip and stuff
+
+//_______________Define-Constants_______________________________________
+//********************************************************************+
+const char* ssid = "LaserScanner";    //Server name/SSID
+const char* password = "3desp32";     //Server password
+IPAddress local_ip(192,168,1,1);      //Sever default ip and stuff
 IPAddress gateway(192,168,1,1); 
 IPAddress subnet(255,255,255,0); 
-WebServer server(80);               //default HTTP-Port
+WebServer server(80);                 //default HTTP-Port
+
+//variables that will change:
+//
+// e.g.: 
+//
 
 
-//Adafruit_VL6180X vl = Adafruit_VL6180X();
+//Adafruit_VL6180X vl = Adafruit_VL6180X();  startet Lib-methoden und funktionen
 
+
+//_________________________________SETUP_________________________________
+//***********************************************************************
 void setup() {
   Serial.begin(115200);
+  //***********Setup Sensor etc..
 
-  //setup Soft Access Point
+  //*******************************
+
+  //***setup Soft Access Point für Webserver-Host auf ESP -> into: softAP_init()
   WiFi.softAP(ssid, password);
   WiFi.softAPConfig(local_ip, gateway, subnet);
   delay(100);
-  
-  server.on("/", handle_OnConnect);     //request zu nach /root abfangen; führt zu Start der StartUp/Kalibrierung im Code
+  //***************************
 
+  //**********Server_handles aufrufen-> into: ON_2_handles()
+  server.on("/", handle_OnConnect);     //request nach /root abfangen; führt zu Start der StartUp/Kalibrierung im Code
   //server.on("/start", handle_startScan); ->zum start scan
-  //server.on("/download", handle_downloadText);  -> measurements herunterladen
-  //server.on("/led2on", handle_led2on);
-  //server.on("/led2off", handle_led2off);
+  server.on("/download", handle_downloadData);  //-> measurements herunterladen
+  //server.on("/led2on", handle_led1on);
+  //server.on("/led2off", handle_led1off);
   server.onNotFound(handle_NotFound); //ungültige URL liefert E404
-  
+  //****************************
+
   server.begin();
   Serial.println("HTTP server started");
 }
 
-void loop() {
-  server.handleClient();
 
-  //HIER: Scan code + Motor
+//_________________________________LOOP_________________________________
+//**********************************************************************
+void loop() {
+  server.handleClient();              //http-Anfragen ausführen mit entsprechenden handles
+
+  //HIER: Scan code + entsprechende Ansteuerung der Motoren, sowie daten speichern usw...
 }
 
+
+//______________________Funktionen-Deklaration_________________________________
+//******************************************************************************
 void handle_OnConnect() {
   //HIER: Start conditions
   Serial.println("Start WebServer zur Kommunikation mit 3D-Laserscanner");
-  server.send(200, "text/html", SendHTML(LED1status,LED2status)); 
+  //SCAN_status from button click Pin???
+  SCAN_status = 1;
+  //SCAN_prog from number of stepps
+  server.send(200, "text/html", SendHTML(SCAN_status)); // SendHTML um Seite für entsprechende Zustände zu generieren
+  //Hier Ausgabe an Bildschirm mit Startup und Initialisierung..
 }
 
-void handle_led1on() {
-  LED1status = HIGH;
-  Serial.println("GPIO4 Status: ON");
-  server.send(200, "text/html", SendHTML(true,LED2status));  //200=OK
-}
+/*void handle_scanStatus() {
+ var SCAN_status init & nutzung usw.....
+}*/
 
-void handle_led1off() {
-  LED1status = LOW;
-  Serial.println("GPIO4 Status: OFF");
-  server.send(200, "text/html", SendHTML(false,LED2status)); 
-}
+void handle_downloadData() {
+  //var SCAN_data init & nutzung usw... mit abfrage Messdaten und Ausgabe
+  SCAN_data = 1;
+  server.send(200, "text/html", SendHTML(SCAN_Data)); --> Neue SendHTML_download nötig 
+} 
 
-void handle_led2on() {
-  LED2status = HIGH;
-  Serial.println("GPIO5 Status: ON");
-  server.send(200, "text/html", SendHTML(LED1status,true)); 
-}
+/*void handle_scanFinished() {
+  .....
+} */
 
-void handle_led2off() {
-  LED2status = LOW;
-  Serial.println("GPIO5 Status: OFF");
-  server.send(200, "text/html", SendHTML(LED1status,false)); 
-}
+/*void handle_scanProg() {
+  var SCAN_prog init & nutzung usw....
+} */
 
-void handle_NotFound(){
-  server.send(404, "text/plain", "Not found");
-}
-
-String SendHTML(uint8_t led1stat,uint8_t led2stat){
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>LED Control</title>\n";
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr +=".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-  ptr +=".button-on {background-color: #3498db;}\n";
-  ptr +=".button-on:active {background-color: #2980b9;}\n";
-  ptr +=".button-off {background-color: #34495e;}\n";
-  ptr +=".button-off:active {background-color: #2c3e50;}\n";
-  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  ptr +="<h1>ESP32 Web Server</h1>\n";
-  ptr +="<h3>Using Access Point(AP) Mode</h3>\n";
-  
-   if(led1stat)
-  {ptr +="<p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";}
-  else
-  {ptr +="<p>LED1 Status: OFF</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";}
-
-  if(led2stat)
-  {ptr +="<p>LED2 Status: ON</p><a class=\"button button-off\" href=\"/led2off\">OFF</a>\n";}
-  else
-  {ptr +="<p>LED2 Status: OFF</p><a class=\"button button-on\" href=\"/led2on\">ON</a>\n";}
-
-  ptr +="</body>\n";
-  ptr +="</html>\n";
-  return ptr;
-}
+//void handle_.....
